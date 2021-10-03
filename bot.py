87b63401861dbb8e42619a1af65b686fe428d78d
Imran95942 <89960334+Imran95942@users.noreply.github.com>
2021-10-03 15:37:21 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,8 +28,8 @@ async def start(event):
   await event.reply("**LaCasade Etiketleme Bot**, Grup veya kanaldaki neredeyse tüm üyelerden bahsedebilirim ★\nDaha fazla bilgi için **/help**'i tıklayın.",
                     buttons=(
                       [Button.url('🌟 Beni Bir Gruba Ekle', 'https://t.me/LaCasadeEtiket_bot?startgroup=a'),
-                      Button.url('📣 Support', 'https://t.me/kurtadamoyunuu'),
-                      Button.url('👮‍♂️ Sahibim', 'https://t.me/Furkanbeyy')]
+                      Button.url('📣 Support', 'https://t.me/isIam01'),
+                      Button.url('👮‍♂️ Sahibim', 'https://t.me/isIam01')]
                     ),
                     link_preview=False
                    )
@@ -39,8 +39,8 @@ async def help(event):
   await event.reply(helptext,
                     buttons=(
                       [Button.url('🌟 Beni Bir Gruba Ekle', 'https://t.me/LaCasadeEtiket_bot?startgroup=a'),
-                       Button.url('📣 Support', 'https://t.me/kurtadamoyunuu'),
-                      Button.url('👮‍♂️ Sahibim', 'https://t.me/Furkanbeyy')]
+                       Button.url('📣 Support', 'https://t.me/isIam01'),
+                      Button.url('👮‍♂️ Sahibim', 'https://t.me/isIam01')]
                     ),
                     link_preview=False
                    )
@@ -106,5 +106,5 @@ async def mentionall(event):
         usrtxt = ""
 
 
-print(">> Bot çalıyor merak etme 👮‍♂️ @Furkanbeyy bilgi alabilirsin <<")
+print(">> Bot çalıyor merak etme 👮‍♂️ @isIam07 bilgi alabilirsin <<")
 client.run_until_disconnected()
